d01f4334ec96d6ac74bf5aa5d9ef1756b82e2998
Octane <32618251+cybort360@users.noreply.github.com>
2024-01-21 21:24:35 +0100

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+const about = document.querySelector('.what');
+const innerLinks = document.querySelector('.about-inner-links');
+
+about.addEventListener('click', () => {
+  innerLinks.classList.toggle('hidden');
+});
