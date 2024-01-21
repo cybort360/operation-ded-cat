6a915bf8a999f9d9c25c9125ab752079f3f43b4a
Octane <32618251+cybort360@users.noreply.github.com>
2024-01-21 22:09:40 +0100

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,10 @@
 const about = document.querySelector('.what');
 const innerLinks = document.querySelector('.about-inner-links');
 
-about.addEventListener('click', () => {
-  innerLinks.classList.toggle('hidden');
+about.addEventListener('mouseover', () => {
+  innerLinks.classList.add('show');
+});
+
+about.addEventListener('mouseout', () => {
+  innerLinks.classList.remove('show');
 });
